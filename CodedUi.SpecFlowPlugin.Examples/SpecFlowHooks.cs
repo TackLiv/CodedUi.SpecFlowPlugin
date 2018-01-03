677dcaa072fe72bbb7377d6f8c088121a74ea749
Zhe Yang <zhe.yang@gdev.biz>
2018-01-03 17:18:50 +0800

    Add hook for CodedUi.Fluent

---
+++ CodedUi.SpecFlowPlugin.Examples/SpecFlowHooks.cs
@@ -0,0 +1,18 @@
+﻿namespace SpecFlow.CodedUI.MsTest.Examples
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using TackLiv.CodedUi.Fluent.Extensions;
+
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public sealed class SpecFlowHooks
+    {
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            ValidateExtension.SetAssertor(Assert.IsTrue);
+        }
+    }
+}
